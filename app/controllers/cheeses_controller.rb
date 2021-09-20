58847fa4ca0c8546c16912bf31710426a56e1f63
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-09-20 10:22:19 -0600

    mm monke

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,10 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using the id from the url
+cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end
+
 end
